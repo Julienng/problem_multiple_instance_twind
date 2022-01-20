c1161e01ba3142e12a8a60674b029e2eefdcad42
julien cavaleiro <julien@cavaleiro.fr>
2022-01-20 16:32:04 +0100

    add example with cra

---
+++ packages/cra/src/App.js
@@ -0,0 +1,12 @@
+import { tw } from "twind";
+import { Input } from "ui";
+
+function App() {
+  return (
+    <div className={tw("max-w-prose mx-auto py-10")}>
+      <Input />
+    </div>
+  );
+}
+
+export default App;
