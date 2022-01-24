27adfd9ee607ada290bc8ec418464b36cc36ff6c
julien cavaleiro <julien@cavaleiro.fr>
2022-01-24 18:49:47 +0100

    correction vitejs configuration

--- packages/vite/vite.config.ts
+++ packages/vite/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "react/jsx-runtime": "react/jsx-runtime.js",
+      "react/jsx-dev-runtime": "react/jsx-dev-runtime.js",
+    },
+  },
+  optimizeDeps: {
+    exclude: ["ui"],
+  },
+});
